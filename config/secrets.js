b4d848d99eb3b2211928ead7a00dd39c8a3bf990
Billy Gingrich <whgingrich@gmail.com>
2020-02-20 17:45:31 -0500

    delete api keys

--- config/secrets.js
+++ config/secrets.js
@@ -1,13 +1,13 @@
 module.exports = {
 
     // google: {
-    //     clientID: '631519036537-42orp2s8ekk5om5ea9irthcgp0e3nkn0.apps.googleusercontent.com',
-    //     clientSecret: 'rJPGMjFg4BcpIHAvN6jDnsQZ'
+    //     clientID: '',
+    //     clientSecret: ''
     // },
 
     // facebook:{
-    //     clientID: '204352224097340',
-    //     clientSecret: 'c9a9ffa3c7c2fb774b3cf87e94fef281'
+    //     clientID: '',
+    //     clientSecret: ''
     // },
 
     jwtSecret: process.env.JWT_SECRET || 'secret code'
