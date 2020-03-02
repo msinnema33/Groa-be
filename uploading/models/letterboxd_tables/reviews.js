ac8a2b5bfd9f5c60a4ef00e112dcc35cd0f540da
cmruss <cmrussell08@gmail.com>
2020-03-02 12:28:49 -0500

    README updated, uploading models do not add duplicates

--- uploading/models/letterboxd_tables/reviews.js
+++ uploading/models/letterboxd_tables/reviews.js
@@ -3,7 +3,21 @@ const db = require("../../../database/dbConfig.js");
 module.exports = { addReview };
 
 async function addReview(review) {
-  const [id] = await db("user_letterboxd_reviews").insert(review, "id");
+  const [id] = await db("user_letterboxd_reviews")
+  .select('*')
+  .where("letterboxd_uri", review.letterboxd_uri)
+  .andWhere("user_id", review.user_id)
+  .then(ratings => {
+    if(ratings.length === 0) {
+      return db("user_letterboxd_reviews")
+      .insert(review, "id");
+    } else {
+      return db("user_letterboxd_reviews")
+      .where("letterboxd_uri", review.letterboxd_uri)
+      .andWhere("user_id", review.user_id)
+      .update("review", review.review)
+    }
+  })
   return db("user_letterboxd_reviews")
     .where({ id })
     .first();
--- uploading/models/letterboxd_tables/watch_list.js
+++ uploading/models/letterboxd_tables/watch_list.js
@@ -3,8 +3,17 @@ const db = require("../../../database/dbConfig.js");
 module.exports = { addToWatchList };
 
 async function addToWatchList(movie) {
-  const [id] = await db("user_letterboxd_watchlist").insert(movie, "id");
+  const [id] = await db("user_letterboxd_watchlist")
+  .select('*')
+  .where("letterboxd_uri", movie.letterboxd_uri)
+  .andWhere("user_id", movie.user_id)
+  .then(watchlist => {
+    if(watchlist.length === 0) {
+      return db("user_letterboxd_watchlist")
+  .insert(movie, "id");
+    }
+  }); 
   return db("user_letterboxd_watchlist")
     .where({ id })
     .first();
-}
+};
