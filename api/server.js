90d9e501c93ddead22d562004fd3c4f9f9b0d1f4
cmruss <cmrussell08@gmail.com>
2020-03-04 19:15:57 -0500

    cors options

--- api/server.js
+++ api/server.js
@@ -8,6 +8,11 @@ const uploadingRouter = require("../uploading/uploading-router.js");
 const server = express();
 
 server.use(helmet());
+server.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "groa.us"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 server.options('*', cors());
 server.use(cors());
 server.use(express.json());
