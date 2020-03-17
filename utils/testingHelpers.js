f24db5242ec32f8927873bf95e7fa1c8b2dcc77c
Brianna Keune <briannakeune@gmail.com>
2020-03-17 17:48:02 -0500

    added a function to truncate tables

---
+++ utils/testingHelpers.js
@@ -0,0 +1,18 @@
+const db = require("../database/dbConfig.js");
+
+module.exports = {
+  prepTestingDB,
+  truncateTable
+};
+
+/**
+ * Preps the testing DB but truncating a choosen table, and running seed data
+ * @param {string} table_name - matches the table name you'd like to truncate
+ */
+async function prepTestingDB(table_name) {
+  await db.raw(`TRUNCATE ${table_name} RESTART IDENTITY CASCADE`);
+}
+
+async function truncateTable(table_name) {
+  await db.raw(`TRUNCATE ${table_name} RESTART IDENTITY CASCADE`);
+}
