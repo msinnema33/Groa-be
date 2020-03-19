6dfac81ba66e81f60d70560d768396f24a48581c
Billy Gingrich <whgingrich@gmail.com>
2020-03-19 18:35:02 -0400

    movie poster URL

--- movies/movies-model.js
+++ movies/movies-model.js
@@ -6,7 +6,7 @@ module.exports = {
 
 async function findMovie(searchTerm, submit, genres2, year2) {
     const movies = await db('imdb_movies')
-        .select('movie_id as id','primary_title as title', 'start_year as year', 'runtime_minutes as runtime', 'genres')
+        .select('movie_id as id','primary_title as title', 'start_year as year', 'runtime_minutes as runtime', 'genres', 'poster_url')
 
     const movies2 = await movies.filter(post => 
         searchTerm !== '' || genres2 ? post.genres.toLowerCase().includes(genres2.toLowerCase()) && post.title.toLowerCase().includes(searchTerm.toLowerCase()) && post.year.toString().toLowerCase().includes(year2.toString().toLowerCase())  : true).map((x, index) =>  {
