5c901ae3d24d06a236f03956e32697574f2782e0
Billy Gingrich <whgingrich@gmail.com>
2020-03-25 13:22:26 -0400

    descending average rating

--- movies/movies-model.js
+++ movies/movies-model.js
@@ -8,15 +8,16 @@ async function findMovie(searchTerm, submit, genres2, year2) {
     const movies = await db('imdb_movies')
         .select('movie_id as id','primary_title as title', 'start_year as year', 'runtime_minutes as runtime', 'genres', 'poster_url', 'average_rating')
         .orderBy('average_rating', 'desc')
-    const movies2 = await movies.filter(post => 
+    if (searchTerm.length >= 4 || submit === true) {
+    const movies2 = movies.filter(x => x.average_rating != null)
+    const movies3 = await movies2.filter(post => 
         searchTerm !== '' || genres2 ? post.genres.toLowerCase().includes(genres2.toLowerCase()) && post.title.toLowerCase().includes(searchTerm.toLowerCase()) && post.year.toString().toLowerCase().includes(year2.toString().toLowerCase())  : true).map((x, index) =>  {
             if( index < 100) {
                 return {Title: x.title, Year: x.year, Runtime: x.runtime, Genres: x.genres, Poster: x.poster_url, Average_Rating: x.average_rating}
             }
         })
-    
-        if (searchTerm.length >= 4 || submit === true) {
-            return movies2.filter(x => x != null || x.Average_Rating != null)}
-
+        const movies4 = await movies3.filter(x => x != null)
+        return movies4
+    }
 } 
 
