9268862cb5e933c0a2d445ebb26e029eb8fec091
Brianna Keune <briannakeune@gmail.com>
2020-03-25 14:31:52 -0500

    updated getWatchlist model

--- uploading/models/user_groa_tables/watch_list.js
+++ uploading/models/user_groa_tables/watch_list.js
@@ -4,25 +4,38 @@ module.exports = { addToWatchList, getWatchlist, getListItemById };
 
 async function addToWatchList(movie) {
   await db("user_groa_watchlist")
-  .select('*')
-  .where("name", movie.name)
-  .andWhere("year", movie.year)
-  .andWhere("user_id", movie.user_id)
-  .then(watchlist => {
-    if(watchlist.length === 0) {
-      return db("user_groa_watchlist")
-      .insert(movie, "id")
-    }
-  }); 
-};
+    .select("*")
+    .where("name", movie.name)
+    .andWhere("year", movie.year)
+    .andWhere("user_id", movie.user_id)
+    .then(watchlist => {
+      if (watchlist.length === 0) {
+        return db("user_groa_watchlist").insert(movie, "id");
+      }
+    });
+}
 
+/**
+ * Returns an array of movie to watch by user_id with their posters
+ * @param {number} user_id - user_groa_watchlist user_id
+ * @returns [{movie},{movie},...]
+ */
 function getWatchlist(user_id) {
-  return db("user_groa_watchlist")
-    .where("user_id", user_id);
-};
+  return db("user_groa_watchlist as wl")
+    .innerJoin("imdb_movies", "imdb_movies.primary_title", "wl.name")
+    .select(
+      "wl.id",
+      "wl.date",
+      "wl.name",
+      "wl.year",
+      "wl.user_id",
+      "imdb_movies.poster_url"
+    )
+    .where("wl.user_id", user_id);
+}
 
 function getListItemById(id) {
   return db("user_groa_watchlist")
-    .where("id", id )
+    .where("id", id)
     .first();
-};
+}
