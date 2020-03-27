08da54fe088e68a494562b8f4a73619bdc4657bd
Brianna Keune <briannakeune@gmail.com>
2020-03-27 16:00:59 -0500

    added docs for users router

--- auth/users/users-router.js
+++ auth/users/users-router.js
@@ -5,34 +5,76 @@ const router = express.Router();
 
 const Users = require("./users-model");
 
-// REGISTER
+/**
+ * @api {post} /api/users/register
+ * @apiName Register a user
+ * @apiGroup Auth
+ *
+ * @apiParam {string} username **Required** | _Unique_ | The desired username of a new user
+ * @apiParam {string} password **Required** | A password of at least 6 characters
+ * @apiParam {string} email  _Unique_ | A valid email of a user
+ *
+ * @apiSuccessExample Success-Response:
+ *  HTTP/1.1 201 Created
+ *  {
+ *    "message": "Registration successful potatochip!",
+ *    "id": 31501823437723
+ *  }
+ *
+ * @apiError UniqueUsernameError The <code>req.body.user_name</code> is already in database.
+ * @apiErrorExample {json} Error-Response:
+ *  HTTP/1.1 400
+ *  {
+ *    errorMessage: "Username already in use!"
+ *  }
+ */
 router.post("/register", (req, res) => {
   let userData = req.body;
   const hash = bcrypt.hashSync(userData.password, 8);
   userData.password = hash;
-  Users.findBy( userData.user_name )
-  .then(user => {
-    if(!user) {
-      Users.add(userData)
-      .then(user => {
+  Users.findBy(userData.user_name)
+    .then(user => {
+      if (!user) {
+        Users.add(userData).then(user => {
           res.status(200).json({
             message: `Registration successful ${user.user_name}!`,
             id: user.id
           });
-      })
-    } else {
-      res.status(400).json({ errorMessage: "Username already in use!" });
-    }
-  })
+        });
+      } else {
+        res.status(400).json({ errorMessage: "Username already in use!" });
+      }
+    })
     .catch(error => {
       res.status(500).json({ errorMessage: "Failed to register new user" });
     });
 });
 
-// LOGIN
+/**
+ * @api {post} /api/users/login
+ * @apiName Login a user
+ * @apiGroup Auth
+ *
+ * @apiParam {string} username **Required** | _Unique_ | Must match an existing user
+ * @apiParam {string} password **Required** | Must match password of username
+ *
+ * @apiSuccessExample Success-Response:
+ *  HTTP/1.1 201 Created
+ *  {
+ *    "message": "Registration successful potatochip!",
+ *    "id": 31501823437723
+ *  }
+ *
+ * @apiError AuthenticationFailed User credentials are not valid.
+ * @apiErrorExample {json} Error-Response:
+ *  HTTP/1.1 400
+ *  {
+ *    "message": "Failed to login"
+ *  }
+ */
 router.post("/login", (req, res) => {
   let { user_name, password } = req.body;
-  Users.findBy( user_name )
+  Users.findBy(user_name)
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
         const token = signToken(user);
